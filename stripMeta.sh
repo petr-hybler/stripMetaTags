5b69de62aa97f9b37fdc2b617c131f655f8b03bb
Petr Hybler <17429498+petr-hybler@users.noreply.github.com>
2021-04-05 09:18:31 +0200

    Update stripMeta.sh

--- stripMeta.sh
+++ stripMeta.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-find /path/to/dir/ -type f -iname "*.mkv" -print0 | while IFS= read -r -d $'\0' file;
+find ./paht/to/dir -type f -iname "*.mp4" -print0 | while IFS= read -r -d $'\0' file;
   do
+    echo "Processing directory: $(dirname "${file}")"
     echo "Processsing file: $(basename "${file}")"
-    ffmpeg -i $file -map_metadata -1 -c:v copy -c:a copy "$(dirname "${file}")/R$(basename "${file}")" < /dev/null
+
+    cp "$file" "tmp.mp4" # copy to avoid "white space" issue
+
+    ######
+    ## don't worry about white spaces in the path/name
+    ####
+    #ffmpeg -i "tmp.mp4" -map_metadata -1 -c:v copy -c:a copy "tmp_no_subs.mp4" < /dev/null ## remove media tags
+    ffmpeg -i "tmp.mp4" -sn -c copy "tmp_no_subs.mp4" < /dev/null ## remove subtitles
+
+    mv "tmp_no_subs.mp4" "$(dirname "${file}")/$(basename "${file}")" # move back to the original directory under the original name
+    rm "tmp.mp4" # cleanup
+
 done
