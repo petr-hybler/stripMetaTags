f1551267e937d8e069dfb4919dcc726f5f375fb2
Petr Hybler <17429498+petr-hybler@users.noreply.github.com>
2021-04-05 09:30:27 +0200

    Update stripMeta.sh

--- stripMeta.sh
+++ stripMeta.sh
@@ -8,9 +8,12 @@ find ./paht/to/dir -type f -iname "*.mp4" -print0 | while IFS= read -r -d $'\0' 
 
     ######
     ## don't worry about white spaces in the path/name
+    ##    use: 
+    ##        2> /dev/null -> to show NO ffmpeg output
+    ##        < /dev/null -> show the ffmpeg output
     ####
-    #ffmpeg -i "tmp.mp4" -map_metadata -1 -c:v copy -c:a copy "tmp_no_subs.mp4" < /dev/null ## remove media tags
-    ffmpeg -i "tmp.mp4" -sn -c copy "tmp_no_subs.mp4" < /dev/null ## remove subtitles
+    #ffmpeg -i "tmp.mp4" -map_metadata -1 -c:v copy -c:a copy "tmp_no_subs.mp4" 2> /dev/null ## remove media tags
+    ffmpeg -i "tmp.mp4" -sn -c copy "tmp_no_subs.mp4" 2> /dev/null ## remove subtitles
 
     mv "tmp_no_subs.mp4" "$(dirname "${file}")/$(basename "${file}")" # move back to the original directory under the original name
     rm "tmp.mp4" # cleanup
